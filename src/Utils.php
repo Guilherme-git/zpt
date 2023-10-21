edbd7166708da8249a8f810990aacfba12a69c4c
Guilherme Ataides <56366253+Guilherme-git@users.noreply.github.com>
2023-10-21 20:57:46 -0300

    Commit branch Questao07

--- src/Utils.php
+++ src/Utils.php
@@ -1,9 +1,12 @@
 <?php
 require_once './User.php';
+require_once './Company.php';
+require_once './Department.php';
 
 function setDb(User\User $user, Company\Company $company, Department\Department $department, $db) {
 	$user->setDb($db);
     $company->setDb($db);
     $department->setDb($db);
 }
+
 ?>
