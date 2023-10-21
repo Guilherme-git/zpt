f1ea77ac15014babb72c1f4d04128e7d2ba2309f
Guilherme Ataides <56366253+Guilherme-git@users.noreply.github.com>
2023-10-20 22:44:18 -0300

    Commit branch Questao04

--- src/Department.php
+++ src/Department.php
@@ -9,5 +9,28 @@ class Department {
 	public function __construct() {
 		$this->user = new User\User(); // @todo fixme
 	}
+
+    public function getLargestDepartmentForEachUser() {
+        $query = "SELECT u.username, d.name AS largest_department
+        FROM user u
+        JOIN user_department ud ON u.id = ud.user
+        JOIN department d ON ud.department = d.id
+        WHERE (d.employees, d.id) IN (
+            SELECT MAX(employees), department
+            FROM department
+            GROUP BY department
+        );
+    ";
+
+        $result = $this->db->q($query);
+
+        $departments = [];
+
+        while ($row = $result->fetch_assoc()) {
+            $departments[$row['username']] = $row['largest_department'];
+        }
+
+        return $departments;
+    }
 }
 ?>
